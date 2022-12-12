b4dc5e959b8b2d2a384e41a0765eaf692bb7f5c8
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2022-12-12 15:20:04 -0600

    Review

--- test/Unosquare.DateTimeExt.Test/Unosquare.DateTimeExt.Test/YearWeek-Tests.cs
+++ test/Unosquare.DateTimeExt.Test/Unosquare.DateTimeExt.Test/YearWeek-Tests.cs
@@ -11,4 +11,32 @@ public class YearWeekTests
         Assert.Equal(new(2022, 1, 2), startDate);
         Assert.Equal(new(2022, 1, 8), endDate);
     }
+
+    [Fact]
+    public void WithWeek_FromDateTime()
+    {
+        var yearMonth = new YearWeek(new(2022, 1, 2));
+
+        Assert.Equal(1, yearMonth.Week);
+        Assert.Equal(2022, yearMonth.Year);
+    }
+
+    [Fact]
+    public void WithWeek_DateRange()
+    {
+        var yearMonth = new YearWeek(1, 2022);
+
+        Assert.Equal(new(2022, 1, 2), yearMonth.StartDate);
+        Assert.Equal(new(2022, 1, 8), yearMonth.EndDate);
+    }
+
+    [Fact]
+    public void WithWeek_DeconstructYearMonth()
+    {
+        var yearMonth = new YearWeek(1, 2022);
+        yearMonth.Deconstruct(out int year, out int week);
+
+        Assert.Equal(1, week);
+        Assert.Equal(2022, year);
+    }
 }
